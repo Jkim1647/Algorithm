d8d365d1a43a36577bf2255139ebab1ab7d8a1fa
Jkim1647 <kjy164777@gmail.com>
2026-08-24 17:13:10 +0900

    [D4] Title: 보석 수집 로봇, Time: 86 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D4/26070. 보석 수집 로봇/보석 수집 로봇.java
+++ SWEA/D4/26070. 보석 수집 로봇/보석 수집 로봇.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -10,9 +9,6 @@ public class Solution {
 	
 	static int T,N,result;
 	static int[][] map;
-	static int[][] rotate;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
 	static Deque<int[]> q;
 	static Deque<int[]> jewel;
 	
@@ -24,7 +20,6 @@ public class Solution {
 			N = Integer.parseInt(br.readLine());
 			
 			map = new int[N][N];
-			rotate = new int[N][N];
 			q = new ArrayDeque<>();
 			jewel = new ArrayDeque<>();
 			
@@ -38,9 +33,6 @@ public class Solution {
 					max_value = Math.max(max_value, map[i][j]);
 				}
 			}
-			//System.out.println("asdasd");
-			//System.out.printf("%d",max_value);
-			//q.add(new int[] {0,0,0}); //x, y, 방향 (오른쪽,아래,왼쪽,위)
 			
 			int index = 1;
 			while(index < max_value) {
@@ -53,13 +45,8 @@ public class Solution {
 						}
 					}
 				}
-				//System.out.println("\nasdasd2");
-				//System.out.printf("%d",index);
 			}
-			
-			//System.out.println("\nasdasd2");
-			//System.out.printf("%d",index);
-			
+
 			if(index == max_value) {
 				for(int i=0;i<N;i++) {
 					for(int j=0;j<N;j++) {
@@ -70,44 +57,8 @@ public class Solution {
 				}
 			}
 			
-			//System.out.println("\nasdasd2");
-			//System.out.printf("%d",index);
-			
-			index = 1;
+			move(0, 0, 0); // 좌표 x,y 방향 1,2,3,4  오른쪽,아래,왼쪽,위
 
-			
-			bfs(0, 0, 0); // 좌표 x,y 방향 1,2,3,4  오른쪽,아래,왼쪽,위
-			
-			/*
-			bfs(int cx, cy, int nx, ny)
-			
-			
-			방향이 오른쪽일때
-			현재 좌표보다 오른쪽인경우 +1
-			방향이 
-					
-			 */
-			/*
-			while(!jewel.isEmpty()) {
-				int temp[] = jewel.poll();
-				sb.append(temp[0])
-				.append(" ")
-				.append(temp[1])
-				.append("\n");
-			}
-			
-			
-			for(int i=0;i<N;i++) {
-				for(int j=0;j<N;j++) {
-					sb.append(map[i][j])
-					.append(" ");
-				}
-				sb.append("\n");
-			}
-			
-			
-			*/
-			
 			sb.append("#")
 			.append(test_case)
 			.append(" ")
@@ -117,36 +68,17 @@ public class Solution {
 		System.out.print(sb);
 	}
 
-	private static void bfs(int cx, int cy, int dirct) {
-		
+	private static void move(int cx, int cy, int dirct) {
 		if(jewel.isEmpty()) {
 			return;
 		}
 		int temp[] = jewel.poll();
-		
-		
+
 		int nx = temp[0];
 		int ny = temp[1];
 		
-		//System.out.printf("%d %d \n",nx,ny);
 		//계산
 		if(dirct == 0) {//오른쪽
-			
-			/*
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 
-			 ->
-			 3 3 3 3 4
-			 3 3 3 3 4
-			 3 3 0 0 0
-			 2 2 1 1 1
-			 2 2 1 1 1
-			 */
-			
 			if(cx == nx && cy <= ny) { //같은 라인 오른쪽
 				result += 0;
 			}else if(cx < nx && cy <= ny) { // 아레인데 오른쪽
@@ -165,21 +97,6 @@ public class Solution {
 			}
 		
 		}else if(dirct == 1) {//아래
-			/*
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 0 0 0 0 0
-			 
-			 ->
-			 2 2 3 3 3
-			 2 2 3 3 3
-			 1 1 0 3 3
-			 1 1 0 3 3
-			 1 1 0 4 4
-			 */
-			
 			if(cx <= nx && cy == ny) {
 				result += 0;
 			}else if(cx <= nx && cy > ny) {
@@ -196,78 +113,44 @@ public class Solution {
 					dirct = 1;
 				}
 			}
-			
-			
 		}else if(dirct == 2) {//왼쪽
-			
-			/*
-			->
-			 1 1 1 2 2
-			 1 1 1 2 2
-			 0 0 0 3 3
-			 4 3 3 3 3
-			 4 3 3 3 3
-			 */
-			
 			if(cx == nx && cy >= ny) {
 				result += 0;
-				//System.out.println("check");
 			}else if(cx > nx && cy >= ny) {
 				result += 1;
 				dirct = 3;
-				//System.out.println("check1");
 			}else if(cx > nx && cy < ny){
 				result += 2;
 				dirct = 0;
-				//System.out.println("check2");
 			}else {
 				result += 3;
 				dirct = 1;
-				//System.out.println("check3");
 				if(ny == 0) {
 					result += 1;
 					dirct = 2;
-					//System.out.println("check4");
 				}
 			}
 		}else if(dirct == 3) {//위
-			/*
-			->
-			 4 4 0 1 1
-			 3 3 0 1 1
-			 3 3 0 1 1
-			 3 3 3 2 2
-			 3 3 3 2 2
-			 */
 			if(cx >= nx && cy == ny) {
 				result += 0;
-				//System.out.println("check");
 			}else if(cx >= nx && cy < ny) {
 				result += 1;
 				dirct = 0;
-				//System.out.println("check1");
 			}else if(cx < nx && cy < ny) {
 				result += 2;
 				dirct = 1;
-				//System.out.println("check2");
 			}else {
 				result += 3;
 				dirct = 2;
-				//System.out.println("check3");
 				if(nx == 0) {
 					result += 1;
 					dirct = 3;
-					//System.out.println("check4");
 				}
 			}
-			
 		}
-		//System.out.println(result);
-		
+
 		//계산 끝
-		bfs(nx,ny,dirct);
+		move(nx,ny,dirct);
 	}
 	
 }
-
-
