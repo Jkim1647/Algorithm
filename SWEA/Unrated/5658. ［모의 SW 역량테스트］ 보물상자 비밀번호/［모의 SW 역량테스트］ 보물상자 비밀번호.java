840044527ed43a13ade162fc639d7bbaefb5014d
Jkim1647 <kjy164777@gmail.com>
2026-08-25 14:34:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 81 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -8,45 +8,46 @@ public class Solution {
 	static StringTokenizer st;
 	
 	static int T,N,K,M;
+	static long result;
+	
+	static TreeSet<Long> set;
 	
 	public static void main(String[] args) throws Exception{
 		T = Integer.parseInt(br.readLine());
 		
 		for(int test_case=1;test_case<=T;test_case++) {
-			
+			result = 0;
+			set = new TreeSet<>(Collections.reverseOrder());
 			st = new StringTokenizer(br.readLine());
 			
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-		
-			M = N/4;
-
-			TreeSet<Long> set = new TreeSet<>(Collections.reverseOrder());
-			String temp = br.readLine();
 			
-			for(int k=0;k<M;k++) {
+			String temp = br.readLine();
+			M = N/4;
+			for(int j=0;j<M;j++) {
 				for(int i=0;i<N;i+=M) {
-					String part = temp.substring(i, i + M);
-                    long value = Long.parseLong(part, 16);
-                    set.add(value);
+					String part = temp.substring(i,i+M);
+					
+					long value = Long.parseLong(part,16);
+					
+					set.add(value);
 				}
 				temp = temp.substring(1) + temp.charAt(0);
 			}
-			
-			long answer = 0;
-			int count = 0;
-			for(long value : set) {
-				if(count == K-1) {
-					answer = value;
-					break;
+
+			int index = 0;
+			for(long t : set) {
+				if(index == K-1) {
+					result = t;
 				}
-				count++;
+				index++;				
 			}
-			
+            
 			sb.append("#")
 			.append(test_case)
 			.append(" ")
-			.append(answer)
+			.append(result)
 			.append("\n");
 		}
 		System.out.print(sb);
